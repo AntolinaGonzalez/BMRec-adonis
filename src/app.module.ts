cd65ec2d9878728dc4b3476a268bf5d1ef44be8e
Antolina Gonzalez <antolina487@gmail.com>
2020-10-05 17:59:42 -0300

    fix: delete unused

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
 @Module({
   imports: [UsersModule,MongooseModule.forRoot('mongodb://localhost/bmrec'), UsersModule],
