30cddd03e2b92ec20b24c5052f56fed0ea7fcfbd
Antolina Gonzalez <antolina487@gmail.com>
2020-10-05 16:25:45 -0300

    fix: delete unnecessary

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/bmrec')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/bmrec'), UsersModule],
   controllers: [AppController],
   providers: [AppService, UsersService],
 })
