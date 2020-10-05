075eacf7149a2843a74f6015844ce69c8244bfe9
Antolina Gonzalez <antolina487@gmail.com>
2020-10-05 17:39:45 -0300

    fix: model

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,10 +2,10 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { CreateUserDto } from "../dto/create-user.dto";
-import { UserDocument, User } from "src/schemas/user.schema";
+import { UserDocument, User, UserSchema } from "src/schemas/user.schema";
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel("User") private UserModel: Model<UserDocument>) {}
+  constructor(@InjectModel("Users") private UserModel: Model<User>) {}
 
   async create(createUser: CreateUserDto){
     const user = new this.UserModel(createUser);
