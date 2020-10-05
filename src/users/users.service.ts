0075c263857d1bfd429f07508f82f15232f14c12
Antolina Gonzalez <antolina487@gmail.com>
2020-10-05 17:25:10 -0300

    feature: create user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { CreateUserDto } from "../dto/create-user.dto";
+import { UserDocument, User } from "src/schemas/user.schema";
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel("User") private UserModel: Model<UserDocument>) {}
+
+  async create(createUser: CreateUserDto){
+    const user = new this.UserModel(createUser);
+    return user.save();
+  }
+}
