fe2c5f9778c51293405942ea171fc26c31e66919
Antolina Gonzalez <antolina487@gmail.com>
2020-10-10 19:47:22 -0300

    feature: sho0w user

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -3,7 +3,7 @@ const User = use("App/Models/BmRecUser");
 const Hash = use("Hash");
 
 class UserController {
-  async store({ request, response }) {
+  async store({ request, response,auth }) {
     const userData = request.only([
       "username",
       "first_name",
@@ -13,28 +13,35 @@ class UserController {
     ]);
     const safePassword = await Hash.make(userData.password);
     const user = await User.create({ ...userData, password: safePassword });
-
+    auth.login(user)
     return response.status(201).json(user);
   }
 
-  async get({ request, response }) {
-
+  async login({ request, response, auth }) {
+    console.log("entro");
     const userData = request.only(["username", "email", "password"]);
-    let user = await User.findBy('username',userData.username);
-    if(!user){
-      user = await User.findBy('email', userData.email)
+    let user = await User.findBy("username", userData.username);
+    console.log('antes se', user)
+    if (!user) {
+      user = await User.findBy("email", userData.email);
     }
-    if(user){
+    if (user) {
       const isSame = await Hash.verify(userData.password, user.password);
-      if(isSame){
-        return response.status(201).json(user);
+      if (isSame) {
+        console.log('el user',user);
+        await auth.attempt(userData.email)
+
+        return 'Logged in successfully'
       }
     }
 
-    return response.status(404)
+    return response.status(404);
   }
-
-  
+  show({ auth, params }) {
+    console.log('show user')
+    console.log(auth.user)
+    return auth.user;
+  }
 }
 
 module.exports = UserController;
