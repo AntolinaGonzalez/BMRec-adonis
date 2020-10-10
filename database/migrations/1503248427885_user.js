4497b20dde512ecec87ed27da71ecb40fcb99bb8
Antolina Gonzalez <antolina487@gmail.com>
2020-10-10 17:11:59 -0300

    feature: user schema

--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -7,9 +7,11 @@ class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments()
-      table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
-      table.string('password', 60).notNullable()
+      table.string("username", 80).notNullable().unique();
+      table.string("email", 254).notNullable().unique();
+      table.string("first_name", 80).notNullable().unique();
+      table.string("last_name", 254).notNullable().unique();
+      table.string("password", 60).notNullable();
       table.timestamps()
     })
   }
