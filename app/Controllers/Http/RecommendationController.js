d882cd58fb270a9147159da838cc2b723e11d0cb
Antolina Gonzalez <antolina487@gmail.com>
2020-10-11 17:24:48 -0300

    feature: recommendations methods

--- app/Controllers/Http/RecommendationController.js
+++ app/Controllers/Http/RecommendationController.js
@@ -1,6 +1,31 @@
-'use strict'
-
+"use strict";
+const Recommendation = use("App/Models/Recommendation");
 class RecommendationController {
+  async store({ request, response, auth }) {
+    const user = auth.user;
+    const recommendationData = request.only(["title", "description"]);
+
+    const recommendation = await Recommendation.create({
+      ...recommendationData,
+      user_id: user.id,
+    });
+
+    return response.status(201).json(recommendation);
+  }
+
+  async update({ request, response,params }) {
+    const recommendation = await Recommendation.find(params.id);
+    const recommendationData = request.only(["title", "description"]);
+    recommendation.merge(recommendationData);
+    recommendation.save();
+
+    return response.status(201).json(recommendation);
+  }
+
+  async delete({request,response,params}){
+    const recommendation = await Recommendation.find(params.id);
+    recommendation.delete()
+  }
 }
 
-module.exports = RecommendationController
+module.exports = RecommendationController;
