c340359541af87719495c2930d0f60741587b158
Antolina Gonzalez <antolina487@gmail.com>
2020-10-11 16:18:47 -0300

    feature: hash password before create

--- app/Models/BmRecUser.js
+++ app/Models/BmRecUser.js
@@ -1,17 +1,26 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
-
-
+const Model = use("Model");
+const Hash = use("Hash");
 class BmRecUser extends Model {
-  static get table () {
-    return 'bm_rec_users'
+  static boot () {
+    super.boot()
+
+    this.addHook('beforeCreate', async (userInstance) => {
+      console.log('lainstance',userInstance.password)
+      userInstance.password = await Hash.make(userInstance.password)
+      console.log('lainstance2',userInstance.password)
+    })
+  }
+  static get table() {
+    return "bm_rec_users";
   }
 
-  static get primaryKey () {
-    return 'id'
+  static get primaryKey() {
+    return "id";
   }
+
 }
 
-module.exports = BmRecUser
+module.exports = BmRecUser;
