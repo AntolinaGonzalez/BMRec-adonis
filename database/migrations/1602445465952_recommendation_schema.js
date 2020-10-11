366a0df5cc04c755dce03cd2299564e668868b43
Antolina Gonzalez <antolina487@gmail.com>
2020-10-11 16:52:22 -0300

    feature: recommendation schema

--- database/migrations/1602445465952_recommendation_schema.js
+++ database/migrations/1602445465952_recommendation_schema.js
@@ -1,19 +1,25 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class RecommendationSchema extends Schema {
-  up () {
-    this.create('recommendations', (table) => {
-      table.increments()
-      table.timestamps()
-    })
+  up() {
+    this.create("recommendations", (table) => {
+      table.increments();
+      table.integer("user_id").unsigned().notNullable();
+      table.string("title", 250).notNullable();
+      table.string("description", 1500).notNullable();
+      table.string("image_url");
+      table.timestamps();
+
+      table.foreign('user_id').references('id').inTable('bm_rec_users');
+    });
   }
 
-  down () {
-    this.drop('recommendations')
+  down() {
+    this.drop("recommendations");
   }
 }
 
-module.exports = RecommendationSchema
+module.exports = RecommendationSchema;
