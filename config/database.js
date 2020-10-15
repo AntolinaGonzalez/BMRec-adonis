1580b172a082115f9e0ca86e610285b966d071cb
Antolina Gonzalez <antolina487@gmail.com>
2020-10-15 15:28:33 -0300

    heroku

--- config/database.js
+++ config/database.js
@@ -47,16 +47,16 @@ module.exports = {
   | npm i --save mysql
   |
   */
-  mysql: {
-    client: 'mysql',
-    connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    }
-  },
+ mysql: {
+  client: 'mysql',
+  connection: {
+    host: Env.get("DB_HOST", CLEARDB_DATABASE_URL.host),
+    port: Env.get("DB_PORT", ""),
+    user: Env.get("DB_USER", CLEARDB_DATABASE_URL.username),
+    password: Env.get("DB_PASSWORD", CLEARDB_DATABASE_URL.password),
+    database: Env.get("DB_DATABASE", CLEARDB_DATABASE_URL.pathname.substr(1)),
+  }
+},
 
   /*
   |--------------------------------------------------------------------------
