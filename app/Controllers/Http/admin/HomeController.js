7676a4dbbe595ad15762ba41f2edc63d97fdb587
Antolina Gonzalez <antolina487@gmail.com>
2020-10-15 00:55:29 -0300

    feature: admin controller

---
+++ app/Controllers/Http/admin/HomeController.js
@@ -0,0 +1,22 @@
+"use strict";
+const Recommendation = use("App/Models/Recommendation");
+
+class HomeController {
+  async index({ view, auth }) {
+    const recommendations = await Recommendation.query().with("user").fetch();
+
+    return view.render("admin.index", {
+      recommendations: recommendations.toJSON(),
+    });
+  }
+
+  async edit({ view, params }) {
+    const recommendation = await Recommendation.find(params.id);
+
+    return view.render("admin.edit", {
+      recommendation: recommendation.toJSON(),
+    });
+  }
+}
+
+module.exports = HomeController;
